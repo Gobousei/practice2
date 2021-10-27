1425ec31f2282cd5e9acfa76d6d93dc23ecb9353
Gobousei <93245017+Gobousei@users.noreply.github.com>
2021-10-27 18:46:27 +0900

    Delete script.js

--- gs/script.js
+++
@@ -1,6 +0,0 @@
-function doGet() {
-  return HtmlService.createTemplateFromFile('siunten')
-        .evaluate()
-      .setTitle("index")
-      .addMetaTag("viewport", "width=device-width,initial-scale=1");
-}
